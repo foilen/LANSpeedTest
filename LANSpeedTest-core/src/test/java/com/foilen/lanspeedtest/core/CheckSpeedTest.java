661c05490c804a249ea2a8fd308c69275c74b2d9
Simon Levesque <simon@simonlevesque.com>
2017-01-08 12:52:25 -0500

    Update copyright year

--- LANSpeedTest-core/src/test/java/com/foilen/lanspeedtest/core/CheckSpeedTest.java
+++ LANSpeedTest-core/src/test/java/com/foilen/lanspeedtest/core/CheckSpeedTest.java
@@ -1,6 +1,6 @@
 /*
     LANSpeedTest https://github.com/foilen/LANSpeedTest
-    Copyright (C) 2016-2016
+    Copyright (C) 2016-2017
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
